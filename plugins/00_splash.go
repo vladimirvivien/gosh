ce9992b01b811c9005935a9308182360ca5784ba
Aeterne Satiatus <merith.tk@gmail.com>
2020-01-10 18:07:37 +0000

    restore original splash

--- plugins/00_splash.go
+++ plugins/00_splash.go
@@ -14,7 +14,20 @@ type splashCmds struct{}
 
 func (t *splashCmds) Init(ctx context.Context) error {
 	// to set your splash, modify the text in the println statement below, multiline is supported
-	fmt.Println(`-------------------------`)
+	fmt.Println(`		
+                        888      	
+                        888      	
+                        888      	
+ .d88b.  .d88b. .d8888b 88888b.  	
+d88P"88bd88""88b88K     888 "88b 	
+888  888888  888"Y8888b.888  888 	
+Y88b 888Y88..88P     X88888  888 	
+ "Y88888 "Y88P"  88888P'888  888 	
+     888                         	
+Y8b d88P                         	
+ "Y88P"	
+ 	
+ `)
 
 	return nil
 }
